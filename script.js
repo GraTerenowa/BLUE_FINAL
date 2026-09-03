2c6da3cd753da01c58919fe3d580659e60ecb7ce
GraTerenowa <kubamazurek@questworld.pl>
2026-09-03 04:44:39 +0200

    Update quest answers in script.js

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ const NEXT_PART_CODE = "1309";
 const STORAGE_KEY = "diamentowy-testament-" + window.location.pathname;
 
 const QUESTS = [
-  { answerType: "text",   answers: ["a"] },   // Quest 1
-  { answerType: "number", answers: ["2"] },   // Quest 2
-  { answerType: "text",   answers: ["c"] },   // Quest 3
-  { answerType: "number", answers: ["4"] },   // Quest 4
-  { answerType: "text",   answers: ["e"] },   // Quest 5
-  { answerType: "number", answers: ["6"] }    // Quest 6
+  { answerType: "text",   answers: ["11"] },   // Quest 1
+  { answerType: "number", answers: ["22"] },   // Quest 2
+  { answerType: "text",   answers: ["33"] },   // Quest 3
+  { answerType: "number", answers: ["44"] },   // Quest 4
+  { answerType: "text",   answers: ["55"] },   // Quest 5
+  { answerType: "number", answers: ["66"] }    // Quest 6
 ];
 
 const questsContainer = document.querySelector("#quests");
